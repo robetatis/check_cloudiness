0bc5994f8ee4aa8581a6ffe884931fee7cb77c38
roberto.tatis <robetatis@yahoo.com>
2023-05-10 22:20:15 +0200

    remove run.bat

--- run.bat
+++
@@ -1,7 +0,0 @@
-python check_cloudiness.py 51.557001189230526 11.14268454213449 get_data berlin
-python check_cloudiness.py 47.85592230372285 2.7479429113743437 get_data france
-python check_cloudiness.py 39.7553319779762 -2.018487007935865 get_data spain
-python check_cloudiness.py 49.57908352097643 29.136779641769518 get_data ukraine
-python check_cloudiness.py -11.19887672891922 -51.56516950499697 get_data brazil
-python check_cloudiness.py -32.5722097952021 -65.4071500035423 get_data argentina
-python check_cloudiness.py 52.13173282075801 -1.1727751934902433 get_data uk
